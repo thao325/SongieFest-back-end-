5528cc0eb6905b57a87842928e67223b21d62e85
Thao Le <thaole0325@gmail.com>
2023-02-02 13:33:38 -0500

    added header, finished styling for songiefest page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Login from "./components/LogIn";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <main className="App">
+      <Header />
       <Login />
       <Footer />
     </main>
