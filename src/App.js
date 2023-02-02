c6e1440127b8bc155016cf123eff1c7ac5b3779f
Thao Le <thaole0325@gmail.com>
2023-02-02 13:07:03 -0500

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/LogIn";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <main className="App">
       <Login />
+      <Footer />
     </main>
   );
 }
--- src/context/AuthProvider.js
+++
@@ -1,7 +0,0 @@
-// import { createContext, useState } from "react";
-
-// const AuthContext = createContext({});
-
-// export const AuthProvider = ({ children }) => {
-
-// }
