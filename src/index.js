5d547847a00ea0f4533177b30ad9b1aef0278fce
Thao Le <thaole0325@gmail.com>
2023-02-03 09:56:18 -0500

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // import reportWebVitals from './reportWebVitals';
 
