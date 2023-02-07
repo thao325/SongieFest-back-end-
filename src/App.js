e042aa642b1a2140ac4411b0964c29bad82eb158
Thao Le <thaole0325@gmail.com>
2023-02-07 10:10:18 -0500

    created functions to convert API data to Json in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import "./App.css";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
@@ -8,12 +9,63 @@ import LoginPage from "./Forms/LoginForm";
 import MusicPostPage from "./Forms/MusicPostPage";
 import HomePage from "./Pages/HomePage";
 import { Register } from "./Forms/Register.jsx";
+import axios from "axios";
 
 const baseUrl = "https://songiefest-be.herokuapp.com/";
 
-// get all music Posts
+// convert API data to Json
+const convertMusicPostFromApi = (apiMusicPost) => {
+  const { id, username, date_published, likes_count } = apiMusicPost;
+  const newMusicPost = { id, username, datePublished, likesCount };
+  return newMusicPost;
+};
+
+const convertCommentFromApi = (apiComment) => {
+  const { id, text, date_published } = apiComment;
+  const newComment = { id, text, datePublished };
+  return newComment;
+};
+
+const convertSongFromApi = (apiSong) => {
+  const { id, title, artist, album, play_count } = apiSong;
+  const newSong = { id, title, artist, album, playCount };
+  return newSong;
+};
+
+const convertUserFromApi = (apiUser) => {
+  const { id, email, username, date_published } = apiUser;
+  const newUser = { id, email, username, datePublished };
+  return newUser;
+};
+
+// ===  get all music posts/ EXPLORE PAGE  ===
+// function not done 
+// const getAllMusicPostsApi = async () => {
+//   try {
+//     const response = await axios.get(`${baseUrl}explore/`);
+//     console.log("API music posts data", response.data);
+//     // convert API data from backend 
+//     return response.data.map(convertMusicPostFromApi);
+//   } catch (error) {
+//     console.log(error);
+//   };
+
+// };
+
+
+
+
+// do we want users to post a music post?
+
+
+
 
 function App() {
+
+
+
+
+
   return (
     <Router>
       <Header />
--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,1 +1,55 @@
+// import React, { useEffect, useState, Component } from "react";
+// import axios from "axios";
 
+// function ExplorePage() {
+//   const [state, setState] = useState([]);
+
+//   const baseUrl = "https://songiefest-be.herokuapp.com/";
+
+//   useEffect(() => {
+//     axios
+//       .get(`${baseUrl}explore/`)
+//       .then((response) => {
+//         console.log("music posts data", response.data);
+//         setState(response.data);
+//         console.log("State", state);
+//       })
+//       .catch((error) => {
+//         console.log("status code", error.response.status);
+//         console.log("error data", error.response.data);
+//       });
+//   }, []);
+
+//   return(
+//   <div>
+//     {state.map((musicPost) => {
+
+//   return(
+
+// <div>
+//       <h1>Latest Music Posts</h1>
+//       {/* while posts are loading, render loading message */}
+//       {loading ? (
+//         <Loader />
+//       ) : // if error, render error message.
+//       // passing error as a child into this component
+//       error ? (
+//         <Message variant="danger">{error}</Message>
+//       ) : (
+//         // if not loading & we don't have an error, render this row back
+//         <Row>
+//           {musicPosts.map((musicPost) => (
+//             <Col key={musicPost.id} sm={12} md={6} lg={4} xL={3}>
+//               <MusicPost musicPost={musicPost} />
+//             </Col>
+//           ))}
+//         </Row>
+//       )}
+//     </div>
+//   );
+// })}
+// </div>
+//   );
+// }
+
+// export default ExplorePage;
