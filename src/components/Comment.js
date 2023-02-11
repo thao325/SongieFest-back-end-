19ed1b1c72a151448976286f577c63ca78fbbef4
Thao Le <thaole0325@gmail.com>
2023-02-11 08:16:17 -0500

    got API call on Comment.js to work- grab username from comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,11 +1,61 @@
-import React from "react";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+//// GET USERNAME ASSOCIATED W A SPECIFIC COMMENT ID
+
+const baseUrl = "https://songiefest-be.herokuapp.com";
+// 'music_post/get-username/<int:commentid>
+
+// takes id, text as props
+function Comment({ id, text }) {
+  const [commentUsername, setCommentUsername] = useState("");
+
+  useEffect(() => {
+    const getCommentUsername = async () => {
+      const cookieValue = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("token="))
+        ?.split("=")[1];
+      const token = "Token " + cookieValue;
+
+      try {
+        const response = await axios.get(`${baseUrl}/music_post/get-username/${id}`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${token}`,
+          },
+        });
+        
+        // if API call success, use setter function to set `commentUsername`
+        // state w/ response data's `username` propery
+        setCommentUsername(response.data.username);
+        // setCommentUsername(response.data);
+        console.log(response.data.username)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getCommentUsername();
+  }, [id]);
+
+  return (
+    <div>
+      <b>{commentUsername} </b>
+      {text}
+    </div>
+  );
+}
 
 
-// const baseUrl = "https://songiefest-be.herokuapp.com";
+export default Comment;
 
 
+
+
+
+
+
+////// /// BIANCA'S CODE BELOW  \\\\\\\\\\\\\\\
 
 // date published willl be added soon once we team up and decide on how we want the time stamp to look like
 //DONT FORGET
@@ -13,7 +63,10 @@ import React from "react";
 // the route is baseurl/music_post/<commentid> 
 //please pull changes to back end i tested it and it returns username  in postman
 // however t im stuck on getting the axios call to retrieve it
-function Comment({id, text}) {
+
+// const baseUrl = "https://songiefest-be.herokuapp.com";
+
+// function Comment({id, text}) {
   // const [commentUsername, setCommentUsername] = useState('');
   // useEffect(() => {
   //   const getMusicPosts = async () => {
@@ -37,15 +90,15 @@ function Comment({id, text}) {
   //   };
   //   getMusicPosts();
   // }, [id]);
-  return (
-    <div>
+//   return (
+//     <div>
       
-      <b>{id} </b>
-      {text}
-      {/* {date_published} */}
+//       <b>{id} </b>
+//       {text}
+//       {/* {date_published} */}
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default Comment;
+// export default Comment;
