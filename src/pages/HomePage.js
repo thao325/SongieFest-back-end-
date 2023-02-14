f8e840a42c4dd0761b69b31188dfd749ebd7c482
Thao Le <thaole0325@gmail.com>
2023-02-13 22:34:54 -0500

    styled & added text to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "../style-sheets/HomePage.css";
 
 function HomePage() {
   return (
-    <div>
-      <h1 className="heading">Welcome to SongieFest</h1>
+    <div className="home-page">
+      <h1 className="home-heading">Welcome to SongieFest</h1>
+      <p className="description">
+        A place to share and discover new music with the world!
+      </p>
+      <p className="sub-description">
+        Join our community today and start sharing your musical journey.
+      </p>
+      <p className="home-heart">
+      ♡
+      </p>
     </div>
   );
 }
